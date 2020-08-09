60c693993172d1184407f99dd0d5d8da8272b13b
Ashish Raina <ashish.raina@bookmyshow.com>
2020-08-10 02:52:50 +0530

    started

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -21,7 +21,9 @@ const isLocalhost = Boolean(
 );
 
 export function register(config) {
-  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+  console.log('process.env.NODE_ENV', process.env.NODE_ENV)
+  if (process.env.NODE_ENV === 'development' && 'serviceWorker' in navigator) {
+    console.log("1a")
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
